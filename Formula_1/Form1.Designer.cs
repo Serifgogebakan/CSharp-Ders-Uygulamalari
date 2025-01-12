0c83a32b663d13be1115489e28dc1e0e88d76b68
MuhammedFatihSaltan <147535753+MuhammedFatihSaltan@users.noreply.github.com>
2025-01-12 17:08:46 +0300

    Proje dosyası ekle.

---
+++ Formula_1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Formula_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Main_panel1 = new System.Windows.Forms.Panel();
+            this.Lane2_pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Lane1_pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.My_Car_pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.My_Car_Velocity_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.My_Car_Movement_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Main_panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Lane2_pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lane1_pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.My_Car_pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Main_panel1
+            // 
+            this.Main_panel1.BackColor = System.Drawing.Color.Silver;
+            this.Main_panel1.Controls.Add(this.Lane2_pictureBox1);
+            this.Main_panel1.Controls.Add(this.Lane1_pictureBox1);
+            this.Main_panel1.Controls.Add(this.My_Car_pictureBox1);
+            this.Main_panel1.Location = new System.Drawing.Point(0, 0);
+            this.Main_panel1.Name = "Main_panel1";
+            this.Main_panel1.Size = new System.Drawing.Size(300, 500);
+            this.Main_panel1.TabIndex = 0;
+            this.Main_panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Main_panel1_Paint);
+            // 
+            // Lane2_pictureBox1
+            // 
+            this.Lane2_pictureBox1.Image = global::Formula_1.Properties.Resources.Ekran_görüntüsü_2024_11_27_215213;
+            this.Lane2_pictureBox1.Location = new System.Drawing.Point(270, -30000);
+            this.Lane2_pictureBox1.Name = "Lane2_pictureBox1";
+            this.Lane2_pictureBox1.Size = new System.Drawing.Size(30, 30500);
+            this.Lane2_pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lane2_pictureBox1.TabIndex = 2;
+            this.Lane2_pictureBox1.TabStop = false;
+            // 
+            // Lane1_pictureBox1
+            // 
+            this.Lane1_pictureBox1.Image = global::Formula_1.Properties.Resources.Ekran_görüntüsü_2024_11_27_214654;
+            this.Lane1_pictureBox1.Location = new System.Drawing.Point(0, -30000);
+            this.Lane1_pictureBox1.Name = "Lane1_pictureBox1";
+            this.Lane1_pictureBox1.Size = new System.Drawing.Size(30, 30500);
+            this.Lane1_pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lane1_pictureBox1.TabIndex = 1;
+            this.Lane1_pictureBox1.TabStop = false;
+            // 
+            // My_Car_pictureBox1
+            // 
+            this.My_Car_pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.My_Car_pictureBox1.Image = global::Formula_1.Properties.Resources.png_transparent_red_bull_racing_car_formula_1_scuderia_ferrari_bahrain_grand_prix_ferrari_formula_1_racing_car_vehicle_thumbnail;
+            this.My_Car_pictureBox1.Location = new System.Drawing.Point(124, 430);
+            this.My_Car_pictureBox1.Name = "My_Car_pictureBox1";
+            this.My_Car_pictureBox1.Size = new System.Drawing.Size(52, 60);
+            this.My_Car_pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.My_Car_pictureBox1.TabIndex = 0;
+            this.My_Car_pictureBox1.TabStop = false;
+            // 
+            // My_Car_Velocity_timer1
+            // 
+            this.My_Car_Velocity_timer1.Enabled = true;
+            this.My_Car_Velocity_timer1.Interval = 500;
+            this.My_Car_Velocity_timer1.Tick += new System.EventHandler(this.My_Car_Velocity_timer1_Tick);
+            // 
+            // My_Car_Movement_timer1
+            // 
+            this.My_Car_Movement_timer1.Enabled = true;
+            this.My_Car_Movement_timer1.Tick += new System.EventHandler(this.My_Car_Movement_timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 503);
+            this.Controls.Add(this.Main_panel1);
+            this.Name = "Form1";
+            this.Text = "Formula 1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Main_panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Lane2_pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lane1_pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.My_Car_pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Main_panel1;
+        private System.Windows.Forms.PictureBox Lane1_pictureBox1;
+        private System.Windows.Forms.PictureBox My_Car_pictureBox1;
+        private System.Windows.Forms.PictureBox Lane2_pictureBox1;
+        private System.Windows.Forms.Timer My_Car_Velocity_timer1;
+        private System.Windows.Forms.Timer My_Car_Movement_timer1;
+    }
+}
+
---
+++ Formula_1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formula_1
+{
+    public partial class Form1 : Form
+    {
+        bool My_Car_Running = false;
+        int My_Car_Velocity = 0;
+        bool My_Car_Stop = true;
+        bool My_Car_Left = false;
+        bool My_Car_Right = false;
+        bool My_Car_Break = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                My_Car_Break = false;
+                My_Car_Running = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                My_Car_Running = false;
+                My_Car_Break = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                My_Car_Right = false;
+                My_Car_Left = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                My_Car_Left = false;
+                My_Car_Right = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                My_Car_Running = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                My_Car_Break = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                My_Car_Left = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                My_Car_Right = false;
+            }
+        }
+        private void Main_panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void My_Car_Velocity_timer1_Tick(object sender, EventArgs e)
+        {
+            if(My_Car_Running && My_Car_Velocity < 80)
+            {
+                My_Car_Velocity = My_Car_Velocity + 5;
+            }
+            if(!My_Car_Running && My_Car_Velocity > 0)
+            {
+                My_Car_Velocity = My_Car_Velocity - 5;
+            }
+            if(My_Car_Break && My_Car_Velocity > 0)
+            {
+                My_Car_Velocity = My_Car_Velocity - 5;
+            }
+            if(My_Car_Velocity == 0)
+            {
+                My_Car_Stop = true;
+            }
+            else
+            {
+                My_Car_Stop = false;
+            }
+        }
+
+        private void My_Car_Movement_timer1_Tick(object sender, EventArgs e)
+        {
+            if (!My_Car_Stop)
+            {
+                Move_Lanes();
+
+                if(My_Car_Left && My_Car_pictureBox1.Left > 30)
+                {
+                    My_Car_pictureBox1.Left = My_Car_pictureBox1.Left - 5;
+                }
+                if(My_Car_Right && My_Car_pictureBox1.Left < 218)
+                {
+                    My_Car_pictureBox1.Left = My_Car_pictureBox1.Left + 5;
+                }
+            }
+        }
+
+        private void Move_Lanes()
+        {
+            Lane1_pictureBox1.Top = Lane1_pictureBox1.Top + My_Car_Velocity;
+            Lane2_pictureBox1.Top = Lane2_pictureBox1.Top + My_Car_Velocity;
+
+        }
+    }
+}
